9f1e6d6ce4373a5df9f24ba9a04de1771942e58b
Alejandro <alejandromontoyacastano51@gmail.com>
2025-02-04 11:06:22 -0500

    quinto commit

--- clase1ej4.py
+++ clase1ej4.py
@@ -1,6 +1,18 @@
+"""
 fruits = ("pera","mango","papaya")
 print(fruits.count("mango"))
 
 lista = [1, 2 ,3 , 4, 5]
 (*x, y, z) = lista
 print(x)
+"""
+"""
+lista = [1,2,3,4,5,6,8,9,0,243,5,78]
+print(f"En la lista hay ", len(lista))
+"""
+lista2 = ["ha", "kajs", "asb", "jh"]
+lista2.insert(0, "lol")
+print(lista2)
+"""
+print(f"en la lista hay, {len(lista2)} numeros")
+"""
